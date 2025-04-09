c1d28da3e90902a1e823af100ea2a9c35786eae2
unknown <panupong-d@rmutp.ac.th>
2025-04-09 19:10:59 +0700

    icon

--- src/common/theme/palette.js
+++ src/common/theme/palette.js
@@ -3,20 +3,20 @@ import { grey, green, indigo } from "@mui/material/colors";
 const validatedColor = (color) =>
   /^#([0-9A-Fa-f]{3}){1,2}$/.test(color) ? color : null;
 
-export default (server, darkMode) => ({
-  mode: darkMode ? "dark" : "light",
+export default (server) => ({
+  mode: "dark",
   background: {
-    default: darkMode ? grey[900] : grey[50],
+    default: grey[900],
   },
   primary: {
     main:
       validatedColor(server?.attributes?.colorPrimary) ||
-      (darkMode ? indigo[200] : indigo[900]),
+      indigo[200],
   },
   secondary: {
     main:
       validatedColor(server?.attributes?.colorSecondary) ||
-      (darkMode ? green[200] : green[800]),
+      green[200],
   },
   neutral: {
     main: grey[500],
